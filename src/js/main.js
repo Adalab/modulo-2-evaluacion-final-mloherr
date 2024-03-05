9ae29b0991bda8d2876a3bf4cd9c5834eb8fbd25
mloherr <herraizmaria08@gmail.com>
2024-03-05 16:46:40 +0100

    localStorage success

--- src/js/main.js
+++ src/js/main.js
@@ -62,48 +62,82 @@ const handleSearch = (event) => {
 
 const handleAddFavorite = (event) => {
   listFavorites.innerHTML = '';
-  console.log(event.currentTarget);
-  console.log(event.target.id);
   const clickedLi = event.currentTarget;
-  console.log(clickedLi);
   clickedLi.classList.add('favoriteAnime');
 
   const animeSelected = showedAnimes.find((anime) => {
     return event.currentTarget.id === anime.mal_id.toString();
   });
-
-  console.log(animeSelected);
-
   const indexFavoritesAnimes = favoritesAnimes.findIndex((favoriteAnime) => {
     return favoriteAnime.mal_id === event.currentTarget.id;
   }); // Si me devuelve -1, añado el elemento al array. Si no, no lo quiero añadir
-  console.log(indexFavoritesAnimes);
   if (indexFavoritesAnimes === -1) {
     favoritesAnimes.push(animeSelected);
   }
 
-  for (const anime of favoritesAnimes) {
-    const animeFavImage = document.createElement('img');
-    if (anime.images.jpg.image_url) {
-      animeFavImage.setAttribute('src', anime.images.jpg.image_url);
-    } else {
-      animeFavImage.setAttribute(
-        'src',
-        'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
-      );
-    }
-    animeFavImage.setAttribute('alt', 'Portada anime');
-    const animeFavTitle = document.createElement('h2');
-    const textFavTitle = document.createTextNode(anime.title);
-    animeFavTitle.appendChild(textFavTitle);
+  renderFavAnimeCard(favoritesAnimes, listFavorites);
+  localStorage.setItem('favoritesAnimes', JSON.stringify(favoritesAnimes));
+};
+
+searchButton.addEventListener('click', handleSearch);
+
+const renderFavImage = (anime) => {
+  const animeFavImage = document.createElement('img');
+  if (anime.images.jpg.image_url) {
+    animeFavImage.setAttribute('src', anime.images.jpg.image_url);
+  } else {
+    animeFavImage.setAttribute(
+      'src',
+      'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+    );
+  }
+  animeFavImage.setAttribute('alt', 'Portada anime');
+  return animeFavImage;
+};
+
+const renderFavTitle = (anime) => {
+  const animeFavTitle = document.createElement('h2');
+  const textFavTitle = document.createTextNode(anime.title);
+  animeFavTitle.appendChild(textFavTitle);
+  return animeFavTitle;
+};
+
+const renderFavAnimeCard = (array, container) => {
+  for (const anime of array) {
+    const animeFavImage = renderFavImage(anime);
+    const animeFavTitle = renderFavTitle(anime);
+    const closeButton = document.createElement('button');
+    closeButton.setAttribute('class', 'js-closeButton');
+    const textButton = document.createTextNode('X');
+    closeButton.appendChild(textButton);
     const newLi = document.createElement('li');
     newLi.setAttribute('class', 'js-itemLi');
     newLi.setAttribute('id', anime.mal_id);
     newLi.appendChild(animeFavTitle);
     newLi.appendChild(animeFavImage);
-    listFavorites.appendChild(newLi);
+    newLi.appendChild(closeButton);
+    container.appendChild(newLi);
   }
-  localStorage.setItem('favoritesAnimes', JSON.stringify(favoritesAnimes));
 };
 
-searchButton.addEventListener('click', handleSearch);
+const showFavAnimeCard = () => {
+  const favoritesAnimesLocalStorage = JSON.parse(
+    localStorage.getItem('favoritesAnimes')
+  );
+
+  if (favoritesAnimesLocalStorage !== null) {
+    renderFavAnimeCard(favoritesAnimesLocalStorage, listFavorites);
+  }
+};
+
+showFavAnimeCard();
+
+const closeButtonElement = document.querySelectorAll('js-closeButton');
+
+const handleClose = (event) => {
+  event.preventDefault();
+};
+
+for (const button of closeButtonElement) {
+  button.addEventListener('click', handleClose);
+}
